51ea1fbd3d6794ac026879ee444bf556cb81bb36
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-09-24 12:14:56 -0500

    added the form inputs and button. Added the main functions needed such as readyNow,addNewEmployee() which has a preventDefault call,render() and clearInputs()

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('in script');
+
+let employees =[];
+
+$(document).ready(readyNow);
+function readyNow(){
+    console.log('in readyNow');
+    //register event handlers here
+}
+function addNewEmployee(evtArgs){
+    evtArgs.preventDefault();
+
+}
+function render(){
+    //render the employees array to the DOM. clear screen and re render all objects everytime a new employee is added
+
+}
+function clearInputs(){
+    //clear input after submit button has been clicked and a employee has been added to global employees array
+
+}
