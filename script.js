de2d561298b5d973b2fa0a1453ec05e2b0679209
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-09-24 12:56:10 -0500

    piped firstname all the way through, addNewEmployee works, it does add, but only if there is input in firstname. it pushes to array and clearInputs() clears firstName input, and render adds to table. need to add clearTable

--- script.js
+++ script.js
@@ -7,18 +7,45 @@ $(document).ready(readyNow); //starting point of script
 function readyNow(){
     console.log('in readyNow');
     //register event handlers here
-    
+    $('#input-Form').on('submit',addNewEmployee);
 }
 function addNewEmployee(evtArgs){
+    //console.log('in addNewEmployee');
     evtArgs.preventDefault();
-    //updates the state ie employees array
+    //updates the state ie employees array, but only if all input fields have info
+    //grab all input
+    let fName =$('#input-firstName').val();
+    
+    // test all input
+    if(fName ===''){
+        //do nothing, could maybe add an alert here, but alerts are disruptive
+        //need to inform user in someway that something is wrong
+        return;
+    }
+    //create new employee object if all input is good
+    let newEmployee = {
+        firstName: fName
+    };
+    //add new employee obj to employees array, ie state
+    employees.push(newEmployee);
+    //since employee was added succesfully, clear all inputs
+    clearInputs();
+    //render the state
+    render();
+
 
 }
 function render(){
     //render the employees array to the DOM. clear screen and re render all objects everytime a new employee is added
-
+    for(let employee of employees){
+        $('#table-Employees').append(`
+            <tr>
+                <td>${employee.firstName}</td>
+            </tr>
+        `);
+    }
 }
 function clearInputs(){
     //clear input after submit button has been clicked and a employee has been added to global employees array
-
+    $('#input-firstName').val('');
 }
